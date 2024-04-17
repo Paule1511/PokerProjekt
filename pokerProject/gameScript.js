d32ac34bdc6b376cfdbb1f1a605e12c908916599
Paule1511 <111686991+Paule1511@users.noreply.github.com>
2024-04-17 14:08:46 +0200

    Add files via upload

--- pokerProject/gameScript.js
+++ pokerProject/gameScript.js
@@ -1,6 +1,5 @@
 const DEFAULT_IMG = "default_player.svg";
 blindIdx = 1;
-pot = 0;
 input = "";
 inputAmount = 0;
 globalBet = 0;
@@ -9,26 +8,36 @@ function sleep(milis){
     return new Promise(resolve => setTimeout(resolve, milis));
 }
 
+function cardToImgNameConv(card){
+    return card.split('').reverse().join('');
+}
+
 class player{
-    #hand = Array(null, null);
+    #hand = Array(0,0);
     #money = 0;
     #imgUrl = DEFAULT_IMG;
+    #isSelf = false;
     #name = "-1";
     #idx = 0;
     #htmlId = '';
-    constructor(money, idx, name = null, img = null){
+    #blind = '';
+    #bet = 0;
+    #state = 'in';
+    constructor(money, idx, name = null, img = null, isSelf = false){
         if(name !== null){
             this.#name = name;
         }
         if(img !== null){
             this.#imgUrl = img;
         }
+        this.#isSelf = isSelf;
         this.#idx = idx;
         this.#money = money;
     }
     setBlind(button){
         const playButtonElement = document.getElementById(this.#htmlId).getElementsByClassName('playButton')[0];
         playButtonElement.innerHTML = button;
+        this.#blind = button;
         switch(button){
             case '':
                 playButtonElement.classList.remove('big');
@@ -43,11 +52,42 @@ class player{
         }
 
     }
+    fold(){
+        this.#state = 'fold';
+        document.getElementById(this.#htmlId).style.background = '#a00';
+    }
+    getState(){
+        return this.#state;
+    }
+    backIn(){
+        this.#state = 'in';
+        document.getElementById(this.#htmlId).style.background = 'rgba(0,0,0,0.35)';
+    }
+    isSelf(){
+        return this.#isSelf;
+    }
+    getBlind(){
+        return this.#blind;
+    }
+    getBet(){
+        return this.#bet;
+    }
+    setBet(bet){
+        if(bet > this.#money){
+            this.#state = 'allIn';
+            this.#bet = this.money;
+            return;
+        }
+        this.#bet = bet;
+    }
     setHTMLID(htmlId){
         this.#htmlId = htmlId;
     }
     getHTMLID(){
         return this.#htmlId;
+    }
+    setHand(idx, card){
+        this.#hand[idx] = card;
     }
     getHand(){
         return this.#hand;
@@ -56,7 +96,7 @@ class player{
         return this.#money;
     }
     removeMoney(amount){
-        this.#money -= amount;
+        return this.#money -= amount;
     }
     addMoney(amount){
         this.#money += amount;
@@ -73,7 +113,6 @@ class player{
     getIndex(){
         return this.#idx;
     }
-
 }
 
 function errorFunction(){
@@ -103,6 +142,10 @@ function drawEnemy(enemy, i, enemCount){
         "<span class='money'>" + enemy.getMoney() + "</span>"+
         "<div class='cards'></div>" +
         "<div class='playButton'></div>" +
+        "<div class='bet' style='visibility: hidden;'>"+
+        "<span class='betText'></span>"+
+        '<img src="jeton.svg" alt="jeton">' +
+        "</div>" +
         "</div>";
     
     htmlElement = document.getElementById('enemy' + i);
@@ -121,6 +164,10 @@ function drawPlayer(player){
         "<span class='money'>" + player.getMoney() + "</span>"+
         "<div class='cards'></div>" +
         "<div class='playButton'></div>" +
+        "<div class='bet' style='visibility: hidden;'>"+
+        "<span class='betText'></span>"+
+        '<img src="jeton.svg" alt="jeton">' +
+        "</div>" +
         "</div>";
     htmlElement = document.getElementById('player');
     htmlElement.style.offsetDistance = 100/10*4+18.5 + "%";
@@ -167,7 +214,7 @@ function showTurn(turn){
     turnElement = document.getElementById('turn')
     switch(turn){
         case 0:
-            turnElement.innerHTML = "Hole Cards";
+            turnElement.innerHTML = "Pre-Flop";
             break;
         case 1:
             turnElement.innerHTML = "Flop";
@@ -195,22 +242,23 @@ function toggleActionButton(button = null){
             actionButtons[j].classList.replace('active', 'unactive');
         }
     }
+
     if(button == null){
         input = '';
+        toggleRaiseBar(false);
         return;
     }
+
     if(button.classList.contains('unactive')){
         if(button.id == 'raise'){
-            toggleRaiseBar();
+            toggleRaiseBar(true);
+        }else{
+            input = button.id;
         }
-        input = button.id;
         button.classList.replace('unactive', 'active');
     }
 }
 
-function processAction(entity){
-    return ['fold', 0];
-}
 
 function setTurn(entity, isTurn){
     htmlElement = document.getElementById(entity.getHTMLID());
@@ -221,54 +269,164 @@ function setTurn(entity, isTurn){
     }
 }
 
+function calculateAction(entity){
+    return ['call', 0];
+}
+
+function drawPlayerCards(entity, canSee){
+    if(canSee){
+        cards = entity.getHand();
+    }else{
+        cards = ['B1', 'B1'];
+    }
+    if(cards[0] != 0 && cards[1] != 0){
+        htmlElement = document.getElementById(entity.getHTMLID()).getElementsByClassName('cards')[0];
+        console.log('url(./cardsSVGs/' + cardToImgNameConv(cards[0]) + '.svg)')
+        htmlElement.style.setProperty('--cardImg1', 'url(./cardsSVGs/' + cardToImgNameConv(cards[0]) + '.svg)');
+        htmlElement.style.setProperty('--cardImg2', 'url(./cardsSVGs/' + cardToImgNameConv(cards[1]) + '.svg)');
+    }
+}
+
 async function getAction(resolve, entity, betTime){
     action = ['fold', 0];
     setTurn(entity, true);
     timer = 0;
     if(entity.getName() == '-1'){
-        action = processAction(entity);
-        await sleep(1000);
+        action = calculateAction(entity);
+        await sleep(500);
     }else{
         while(input == '' && timer <= betTime*1000){
             await sleep(10);
             timer+=10;
         }
-        action = [input, inputAmount];
+        action = [input, parseInt(inputAmount)];
         toggleActionButton();
     }
     setTurn(entity, false);
     resolve(action);
 }
 
+function updateBets(entityList){
+    for(j = 0; j < entityList.length; j++){
+        htmlElement = document.getElementById(entityList[j].getHTMLID());
+        htmlElement.getElementsByClassName('betText')[0].innerHTML = entityList[j].getBet();
+        if(entityList[j].getBet() > 0){
+            htmlElement.getElementsByClassName('bet')[0].style.visibility = 'visible';
+        }else{
+            htmlElement.getElementsByClassName('bet')[0].style.visibility = 'hidden';
+        }
+    }
+}
 
+function updateMoney(entityList){
+    for(let i = 0; i < entityList.length; i++){
+        htmlElement = document.getElementById(entityList[i].getHTMLID());
+        htmlElement.getElementsByClassName('money')[0].innerHTML = entityList[i].getMoney();
+    }
+}
+
+function updateEntitys(entityList){
+    updateMoney(entityList);
+    updateBets(entityList);
+}
+
+function processAction(entity, action, bet){
+    switch(action[0]){
+        case 'fold', '':
+            entity.fold();
+            break;
+        case 'call':
+            entity.setBet(bet);
+            break;
+        case 'raise':
+            entity.setBet(action[1]);
+            break;
+    }
+    return entity.getBet();
+}
+
+function updatePot(pot){
+    document.getElementById('potAmount').innerHTML = pot;
+}
+
+function updateTable(entityList, pot){
+    for(j = 0; j < entityList.length; j++){
+        entityList[j].setBet(0);
+    }
+    updateEntitys(entityList);
+    updatePot(pot);
+}
+
+function removeAllCards(entityList){
+    for(j = 0; j < entityList.length; j++){
+        htmlElement = document.getElementById(entityList[j].getHTMLID());
+        htmlElement.style.setProperty('--cardImg1', '');
+        htmlElement.style.setProperty('--cardImg2', '');
+    }
+}
+
+function giveCards(deck, entityList, handIdx){
+    for(i = 0; i < entityList.length; i++){
+        entityList[i].setHand(handIdx, deck.shift());
+    }
+}
 
 async function game(resolve, entityList, blind){
+    pot = 0;
     turn = 0;
     bet = blind;
     globalBet = bet;
     sortByEntityIdx(entityList);
     setEntityBlinds(entityList);
-    while(turn < 5){
-        lastBetIdx = 0;
+    for(i = 0; i < entityList.length; i++){
+        if(entityList[i].getBlind() == 'BB'){
+            entityList[i].setBet(blind);
+        }else if(entityList[i].getBlind() == 'SB'){
+            entityList[i].setBet(blind/2);
+        }
+    }
+    giveCards(deck, entityList, 0);
+    giveCards(deck, entityList, 1);
+    for(i = 0; i < entityList.length; i++){
+        if(entityList[i].isSelf()){
+            drawPlayerCards(entityList[i], true);
+        }else{
+            drawPlayerCards(entityList[i], false);
+        }
+    }
+    lastBetIdx = blindIdx+1;
+    startIdx = blindIdx+1;
+    while(turn < 4){
         showTurn(turn);
-        for(i = blindIdx; i < entityList.length+blindIdx+lastBetIdx; i++){
-            currAction = new Promise(resolve => getAction(resolve, entityList[(i+1)%entityList.length], 5));
+        for(i = startIdx; i < entityList.length+lastBetIdx; i++){
+            let entity = entityList[i%entityList.length];
+            updateEntitys(entityList);
+            if(entity.getState() == 'fold'){
+                continue;
+            }
+            currAction = new Promise(resolve => getAction(resolve, entity, 5));
             action = await currAction.then(function(out){ return out});
+            
             if(action[0] == 'raise'){
                 lastBetIdx = i;
                 bet = action[1];
                 globalBet = bet;
             }
+            pot += processAction(entity, action, bet);
+            entity.removeMoney(entity.getBet());
         }
-        turn++;
+        updateTable(entityList, pot);
         bet = 0;
+        startIdx = 0;
+        lastBetIdx = 0;
+        turn++;
     }
     resolve();
 }
 
-function toggleRaiseBar(){
+function toggleRaiseBar(on){
     raiseBar = document.getElementById('raiseBar');
-    if(raiseBar.style.visibility == 'hidden'){
+    if(on){
         raiseBar.style.visibility = 'visible';
     }else{
         raiseBar.style.visibility = 'hidden';
@@ -292,15 +450,25 @@ function addListeners(){
     raiseConfirm = document.getElementById('raiseConfirm');
     raiseConfirm.addEventListener('click', function(){
         inputAmount = raiseRange.value;
-        toggleRaiseBar();
+        input = 'raise'
+        toggleRaiseBar(false);
     })
+}
 
+function cloneArray(arr){
+    return arr.map((x) => x);
+}
+
+function backIntoGame(entityList){
+    for(j = 0; j < entityList.length; j++){
+        entityList[j].backIn();
+    }
 }
 
 addEventListener('DOMContentLoaded', function(){
     const params = new URLSearchParams(window.location.search);
     enemies = createBots(parseInt(params.get('enemCount')));
-    self = new player(1000, 5, 'Paul'); 
+    self = new player(1000, 5, 'Paul', null, true); 
     if(enemies == null){
         errorFunction();
         return;
@@ -311,11 +479,16 @@ addEventListener('DOMContentLoaded', function(){
     document.getElementById('start').addEventListener('click',async function(){
         this.parentElement.removeChild(this);
         while(true){
+            deck = getNewDeck();
+            console.log(deck);
             blind = 50;
             currGame = new Promise(resolve => game(resolve, entityList, blind));
             await currGame
-            blindIdx = (blindIdx)%entityList.length+1;
+            blindIdx = (++blindIdx)%entityList.length;
+            removeAllCards(entityList);
+            backIntoGame(entityList);
             await sleep(1000);
         }
     })
+    console.log("test")
 })
